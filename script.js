3abcffc77bc8fe155d21e60aff1d3540acf60cdb
Julia <julia.green@skola.taby.se>
2024-04-23 15:08:07 +0200

    Improved colliders, made sprites sort, and added to debugmode

--- script.js
+++ script.js
@@ -189,27 +189,26 @@ class BoxCollider {
   checkCollision(axis) {
     gameManager.colliders.forEach((collider) => {
       if (gameManager.debugMode) {
+        //Draws the outlines of all colliders if in debug mode
+        ctx.globalCompositeOperation = "destination-over"; // Makes it so the outline appears above the sprites despite being drawn first
+        ctx.strokeStyle = "#00FF00";
         ctx.beginPath();
         ctx.strokeRect(
-          collider.getPos("x") + collider.offset.x,
-          collider.getPos("y") + collider.offset.y,
+          collider.getPos("x"),
+          collider.getPos("y"),
           collider.width,
           collider.height
         );
         ctx.stroke();
       }
-      if (collider === this) return;
+      if (collider === this) return; //We do not want to compare with ourselves
       if (
         // Is the collider within the same x as the moving object's collider
-        this.getPos("x") + this.offset.x <
-          collider.getPos("x") + collider.offset.x + collider.width &&
-        this.getPos("x") + this.offset.x + this.width >
-          collider.getPos("x") + collider.offset.x &&
+        this.getPos("x") < collider.getPos("x") + collider.width &&
+        this.getPos("x") + this.width > collider.getPos("x") &&
         // Is the collider within the same y as the moving object's collider
-        this.getPos("y") + this.offset.y <
-          collider.getPos("y") + collider.offset.y + collider.height &&
-        this.getPos("y") + this.offset.y + this.height >
-          collider.getPos("y") + collider.offset.y
+        this.getPos("y") < collider.getPos("y") + collider.height &&
+        this.getPos("y") + this.height > collider.getPos("y")
       )
         if (this.isTrigger) {
           //Put code for onTrigger
@@ -220,7 +219,7 @@ class BoxCollider {
     });
   }
   getPos(axis = "") {
-    if (axis) return this.gameObject.pos[axis];
+    if (axis) return this.gameObject.pos[axis] + this.offset[axis];
     else return this.gameObject.pos;
   }
 }
@@ -286,7 +285,7 @@ class Player extends Entity {
     );
     this.addComponent(Animator, this);
     this.addComponent(Controller, "d", "a", "w", "s");
-    this.addComponent(BoxCollider, 13, 22, new Vector2(18, 19));
+    this.addComponent(BoxCollider, 13, 5, new Vector2(18, 35));
 
     this.movingRight = false;
     this.movingLeft = false;
@@ -321,19 +320,31 @@ class Player extends Entity {
 //Game specific variables------------------------------------------------------------------------------------------
 
 let player = new Player();
-player.instantiate(new Vector2(300, 100));
+player.instantiate(new Vector2(250, 100));
 
 let slime = new Entity(
   new SpriteSheet("characters/slime.png", 32, 32, 4),
   new SpriteSheet("characters/slime.png", 32, 32, 4)
 );
-slime.addComponent(Animator, slime.idleSpritesheet);
-slime.instantiate(new Vector2(100, 100));
 
 let counter = new GameObject();
 counter.addComponent(Animator, new SpriteSheet("objects/counter.png", 78, 33));
-counter.addComponent(BoxCollider, 78, 33);
+counter.addComponent(BoxCollider, 78, 17, new Vector2(0, 16));
+counter.addComponent(BoxCollider, 15, 16, new Vector2(63, 0));
 counter.instantiate(new Vector2(200, 200));
+
+if (gameManager.debugMode) {
+  addEventListener("mousedown", function (e) {
+    //Add so you know where your mouse has clicked
+    var rect = canvas.getBoundingClientRect();
+    console.log(
+      "Clicked x:",
+      e.clientX - rect.left,
+      "y:",
+      e.clientY - rect.top
+    );
+  });
+}
 
 let lastTimestamp = 0,
   maxFPS = 10,
@@ -346,11 +357,19 @@ function update(timestamp) {
     player.speed
   );
   player.chooseAnimation();
+  gameManager.instantiated.sort(
+    //Sortera object i instantiated så att objekten med högst y animeras sist
+    (a, b) =>
+      b.pos.y +
+      b.getComponent(Animator).spriteSheet.sHeight -
+      (a.pos.y + a.getComponent(Animator).spriteSheet.sHeight)
+  );
   gameManager.instantiated.forEach((obj) => {
     obj.activeComponents.forEach((component) => {
       component.run(timestamp);
     });
   });
+
   if (!(timestamp - lastTimestamp < timestep)) {
     lastTimestamp = timestamp;
   }
